aa321fbb0c87e94c25bb578fa3c89636b021fd41
Anshul Kumar <f20230621@goa.bits-pilani.ac.in>
2024-06-15 23:08:10 +0530

    Now People can eb registered

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-mongoose.connect(
-    "mongodb+srv://ansh7026:sgSjYzg0Z3LYXeNc@cluster0.ynj7jhm.mongodb.net/bankdata/"
-);
-
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+mongoose.connect(
+    "mongodb+srv://ansh7026:sgSjYzg0Z3LYXeNc@cluster0.ynj7jhm.mongodb.net/bankdata",
+);
 
 app.listen(5500, function () {
     console.log("Server is running on port 5500");
@@ -19,38 +20,32 @@ const User = mongoose.model("User", {
     password: String
 });
 
+
+
 app.post("/Registration", function (req, res) {
+
     try {
         const newUser = new User({
             name: req.body.name,
             email: req.body.email,
             password: req.body.password
         });
-        newUser.save();
-        res.redirect("http://127.0.0.1:5501/Login.html");
+        const foundUser = User.findOne({ email: req.body.email });
+        if(foundUser){
+            res.redirect("http://127.0.0.1:5501/Registration.html");
+        }
+        else{
+            newUser.save();
+            console.log("New user saved:", newUser);
+            res.redirect("http://127.0.0.1:5501/Login.html");
+        }
+        
+
     } catch (error) {
         console.error("Error saving new user:", error);
         res.status(500).send("Internal Server Error");
     }
 });
 
-app.post("/Login", async function (req, res) {
-    const loginUsername = JSON.stringify(req.body.loginusername);
-    const loginPassword = JSON.stringify(req.body.loginpassword);
-    try {
-        const foundUser = await User.findOne({ email: loginUsername });
 
-        if (foundUser) {
-            if (foundUser.password !== loginPassword) {
-                res.status(401).send("Invalid Password");
-            } else {
-                res.redirect("http://127.0.0.1:5501/userProfile.html");
-            }
-        } else {
-            res.status(404).send("User not found");
-        }
-    } catch (error) {
-        console.error("Error during login:", error);
-        res.status(500).send("Internal Server Error");
-    }
-});
+
