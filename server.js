59835fed5c365466392b58ab50a619ea43e789cb
Anshul Kumar <f20230621@goa.bits-pilani.ac.in>
2024-06-25 23:09:04 +0530

    Made a functional login registrational website

--- server.js
+++ server.js
@@ -10,16 +10,28 @@ mongoose.connect(
     "mongodb+srv://ansh7026:sgSjYzg0Z3LYXeNc@cluster0.ynj7jhm.mongodb.net/bankdata",
 );
 
-app.listen(5500, function () {
-    console.log("Server is running on port 5500");
+app.listen(3000, function () {
+    console.log("Server is running on port 3000");
 });
+
+app.get("/Login.html", function (req, res) {
+    res.sendFile(__dirname + "/Login.html");
+});
+
+app.get("/Registration.html", function (req, res) {
+    res.sendFile(__dirname + "/Registration.html");
+});
+
+app.get("/userProfile.html" , function(req,res){
+    res.sendFile(__dirname + "/userProfile.html");
+});
+
 
 const User = mongoose.model("User", {
     name: String,
     email: String,
     password: String
 });
-
 
 async function findUser(email_in) {
     const user = await User.findOne({ email: email_in }).exec();
@@ -43,10 +55,31 @@ app.post("/Registration", async function (req, res) {
             });
             newUser.save();
             console.log("New user saved:", newUser);
-            res.redirect("http://127.0.0.1:5501/Login.html");
+            res.redirect("http://localhost:3000/Login.html");
         }
         
 
+    } catch (error) {
+        console.error("Error saving new user:", error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+app.post("/Login", async function (req, res) {
+    try {
+        let foundUser = await findUser(req.body.email);
+        if(foundUser != null){
+            if(foundUser.password == req.body.password){
+                res.redirect("http://localhost:3000/userProfile.html");
+            }
+            else{
+                res.send("Incorrect Password");
+            }
+        }
+        else{
+            res.send("User not found");
+            console.log(req.body);
+        }
     } catch (error) {
         console.error("Error saving new user:", error);
         res.status(500).send("Internal Server Error");
